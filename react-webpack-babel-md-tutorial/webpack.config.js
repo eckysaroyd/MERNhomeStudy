d6d475048d2b16a4286db8cf66a7301c67fc42f0
eckysaroyd <enyato98@gmail.com>
2019-10-01 04:01:27 +0530

    changing of sample codes

---
+++ react-webpack-babel-md-tutorial/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.export = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "index.bundle.js"
+  },
+  mode: process.env.NODE_ENV || "development",
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "src")
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html")
+    })
+  ]
+};
