11f5ecd268668b24963f711a6447bba0444f676f
eckysaroyd <enyato98@gmail.com>
2019-10-01 01:55:45 +0530

    new changes are available

--- react-webpack-babel-md-tutorial/src/webpack.config.js
+++ react-webpack-babel-md-tutorial/src/webpack.config.js
@@ -1,21 +1,23 @@
 const path = require("path");
-const htmlwebpackplugin = require("html-webpack-plugin");
-module.export = {
-  entry: path.join(_dirname, "src", "index.js"),
+
+const  htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.export ={
+  entry: path.join(_dirname,"src","index.js"),
   output: {
-    path: path.join(_dirname, "build"),
+    path: path.join(_dirname,"build"),
     filename: "index.bundle.js"
   },
   mode: process.env.NODE_ENV || "development",
   resolve: {
-    modules: [path.resolve(_dirname, "src"), "node_modules"]
+    modules: [path.resolve(_dirname,"src"),"node_modules"]
   },
   devServer: {
-    ContentBase: path.join(_dirname, "src")
+    contentBase: path.join(_dirname,"src")
   },
   plugins: [
-    new htmlwebpackplugin({
-      template: path.join(_dirname, "src", "index.html")
+    new htmlWebpackPlugin({
+      template: path.join(_dirname,"src","index.html")
     })
   ]
 };
