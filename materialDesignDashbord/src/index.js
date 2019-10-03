b0ec40eab6f05185ba75627f0e9bc5f9b4bb9ffe
eckysaroyd <enyato98@gmail.com>
2019-10-04 01:38:35 +0530

    new changes

--- materialDesignDashbord/src/index.js
+++ materialDesignDashbord/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 // this line is new
 // we now have some nice styles on our react app
-// import "index.scss";
+import "index.scss";
 let HelloWorld = () => {
   return <h1>Hello there World!</h1>;
 };
