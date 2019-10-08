3a4202c8d6cd9012cb16c2935eac89059bad1298
eckysaroyd <enyato98@gmail.com>
2019-10-08 07:47:34 +0530

    new changes add see it also in plunker

---
+++ simpleReactApp-PlukerEditor/starter3/script.jsx
@@ -0,0 +1,21 @@
+// Code goes here through plunker
+
+var Product = React.createClass({
+  getInitialState: function() {
+    return { qty: 0 };
+  },
+  buy: function() {
+    this.setState({ qty: this.state.qty + 1 });
+  },
+  render: function() {
+    return (
+      <div>
+        <p>100$</p>
+        <button onClick={this.buy}> Buy</button>
+        <h3>Qty:{this.state.qty} item(s)</h3>
+      </div>
+    );
+  }
+});
+
+React.render(<Product />, document.getElementById("root"));
