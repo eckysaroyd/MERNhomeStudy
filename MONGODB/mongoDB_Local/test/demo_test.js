1c528454c992fc333efed65f183bf4fa22102fe7
eckysaroyd <enyato98@gmail.com>
2019-10-25 13:33:02 +0530

    new testing code

---
+++ MONGODB/mongoDB_Local/test/demo_test.js
@@ -0,0 +1,10 @@
+var mocha = require("mocha");
+var assert = require("assert");
+
+//describe test
+describe("some demo tests", function() {
+  //create tests
+  it("adds two number together", function() {
+    assert(2 + 3 === 5);
+  });
+});
